1897c088ddf87bd8067b2fadf414ce9dbb941ae2
Sam <lemonverse@gmail.com>
2017-12-21 11:51:51 +1300

    Small hole polish

--- src/main/java/party/lemons/arcane/spell/SpellEmergencyHole.java
+++ src/main/java/party/lemons/arcane/spell/SpellEmergencyHole.java
@@ -7,6 +7,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import party.lemons.arcane.api.spell.Spell;
 import party.lemons.arcane.api.spell.SpellPage;
@@ -27,7 +28,6 @@ public class SpellEmergencyHole extends Spell
 	{
 		if(!canCast(player))
 			return;
-
 		if(!player.world.isRemote)
 		{
 			Vec3d vec3 = player.getPositionEyes(1F);
@@ -52,12 +52,21 @@ public class SpellEmergencyHole extends Spell
 						world.setBlockToAir(pos1);
 						world.setBlockToAir(pos2);
 
-						((EntityPlayerMP)player).connection.setPlayerLocation(pos2.getX(), pos2.getY(), pos2.getZ(), ((EntityPlayerMP) player).rotationYaw, ((EntityPlayerMP) player).rotationPitch);
+						((EntityPlayerMP) player).connection.setPlayerLocation(pos2.getX(), pos2.getY(), pos2.getZ(), ((EntityPlayerMP) player).rotationYaw, ((EntityPlayerMP) player).rotationPitch);
+						useMana(player);
+						//TODO: Sound/Particles
 					}
+					else
+					{
+						player.sendStatusMessage(new TextComponentTranslation("spell.hole.fail"), true);
+					}
+				}
+				else
+				{
+					player.sendStatusMessage(new TextComponentTranslation("spell.hole.fail"), true);
 				}
 			}
 		}
-		useMana(player);
 	}
 
 	private boolean canPlace(BlockPos pos, IBlockState state, World world, EntityPlayer player)
